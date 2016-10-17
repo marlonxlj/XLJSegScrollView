762667400775b12e5cc04c0384319f892c23a4ba
marlonxlj <x@xlj.local>
2016-10-17 17:10:19 +0800

    工程创建提交

---
+++ XLJSegScrollView/Classes/Category/UIBarButtonItem+XLJButtonItem.h
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+XLJButtonItem.h
+//  XLJSegScrollView
+//
+//  Created by m on 2016/10/17.
+//  Copyright © 2016年 XLJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBarButtonItem (XLJButtonItem)
+
+@end
---
+++ XLJSegScrollView/Classes/Category/UIBarButtonItem+XLJButtonItem.m
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+XLJButtonItem.m
+//  XLJSegScrollView
+//
+//  Created by m on 2016/10/17.
+//  Copyright © 2016年 XLJ. All rights reserved.
+//
+
+#import "UIBarButtonItem+XLJButtonItem.h"
+
+@implementation UIBarButtonItem (XLJButtonItem)
+
+@end
